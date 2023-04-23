68fdb0341942bdc32f8668aa3d34727e3b9387b5
himadribh07 <hbhattacharya3@gmail.com>
2023-04-24 00:50:56 +0530

    scrolling isssue #2

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -4,32 +4,31 @@ import { useParams } from "react-router-dom";
 import Cards from "../card/card";
 
 const MovieList = () => {
-  const [movieList, setMovieList] = useState([]);
-  const { type } = useParams();
+    const [movieList, setMovieList] = useState([]);
+    const { type } = useParams();
 
-  useEffect(() => {
-    const getData = () => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/${
-          type ? type : "popular"
-        }?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
-      )
-        .then((res) => res.json())
-        .then((data) => setMovieList(data.results));
-    };
-    getData();
-  }, [type]);
+    useEffect(() => {
+        const getData = () => {
+            fetch(
+                `https://api.themoviedb.org/3/movie/${type ? type : "popular"
+                }?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
+            )
+                .then((res) => res.json())
+                .then((data) => setMovieList(data.results));
+        };
+        getData();
+    }, [type]);
 
-  return (
-    <div className="movie__list">
-      <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
-      <div className="list__cards">
-        {movieList.map((movie) => (
-          <Cards movie={movie} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="movie__list">
+            <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
+            <div className="list__cards">
+                {movieList.map((movie) => (
+                    <Cards movie={movie} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default MovieList;
--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -6,17 +6,27 @@ const Movie = () => {
     const [currentMovieDetail, setMovie] = useState()
     const { id } = useParams()
 
-    useEffect(() => {
-        getData()
-        window.scrollTo(0,0)
-    }, [])
+    // useEffect(() => {
+    //     getData()
+    //     window.scrollTo(0,0)
+    // }, [])
 
-    const getData = () => {
+    // const getData = () => {
+    //     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+    //     .then(res => res.json())
+    //     .then(data => setMovie(data))
+    // }
+    const getData = useCallback(() => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
         .then(res => res.json())
-        .then(data => setMovie(data))
-    }
-
+        .then(data => setMovie(data));
+      }, [id]);
+      
+      useEffect(() => {
+        getData();
+        window.scrollTo(0,0);
+      }, [getData]);
+      
     return (
         <div className="movie">
             <div className="movie__intro">

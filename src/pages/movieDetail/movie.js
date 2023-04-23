012eddb0ff4118183212509c40ea6bd400a760aa
himadribh07 <hbhattacharya3@gmail.com>
2023-04-24 00:52:27 +0530

    scrolling issue #3 : solved

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -16,16 +16,16 @@ const Movie = () => {
     //     .then(res => res.json())
     //     .then(data => setMovie(data))
     // }
-    const getData = useCallback(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res => res.json())
-        .then(data => setMovie(data));
-      }, [id]);
-      
-      useEffect(() => {
+    useEffect(() => {
+        const getData = () => {
+          fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+          .then(res => res.json())
+          .then(data => setMovie(data));
+        };
         getData();
         window.scrollTo(0,0);
-      }, [getData]);
+      }, []);
+      
       
     return (
         <div className="movie">

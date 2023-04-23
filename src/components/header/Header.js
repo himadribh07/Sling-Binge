9ed70711bab54a70ae37f4f1dad28505086f9b4e
himadribh07 <hbhattacharya3@gmail.com>
2023-04-24 01:41:33 +0530

    logo changes #1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" alt="IMDB icon" /></Link>
+                <Link to="/"><img className="header__icon" src="/sling-binge-high-resolution-color-logo.png" alt="IMDB icon" /></Link>
                 <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>

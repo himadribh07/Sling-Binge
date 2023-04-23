4ee561819b030f97734232d890d7630b496a669e
himadribh07 <hbhattacharya3@gmail.com>
2023-04-24 00:31:05 +0530

    changes #5

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -8,6 +8,12 @@ const MovieList = () => {
     const [movieList, setMovieList] = useState([])
     const {type} = useParams()
 
+   
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieList(data.results))
+    }
     useEffect(() => {
         getData()
     }, [getData])
@@ -16,11 +22,6 @@ const MovieList = () => {
         getData()
     }, [type])
 
-    const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res => res.json())
-        .then(data => setMovieList(data.results))
-    }
 
     return (
         <div className="movie__list">

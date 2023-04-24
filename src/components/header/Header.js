a3fe6a2637040506e85140cd554d8b978f4c8ca6
himadribh07 <hbhattacharya3@gmail.com>
2023-04-24 10:43:50 +0530

    minor chanages #1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="header">
             <div className="headerLeft">
                 <Link to="/"><img className="header__icon" src="/sling-binge-high-resolution-color-logo.png" alt="IMDB icon" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Trending</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
             </div>
